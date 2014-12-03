ed412c41b175ac0ab3718b48da28bbb0bf076a42
Shareef Ali <shareefalis@gmail.com>
2014-12-03 02:01:20 -0500

    Fix board-specific HAL module naming
    Change-Id: I3173018360c46cf8bc3791dbe7e43b649a704db0

--- halimpl/pn54x/Android.mk
+++ halimpl/pn54x/Android.mk
@@ -12,9 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+ifneq ($(BOARD_NFC_HAL_SUFFIX),)
+    HAL_SUFFIX := pn54x.$(BOARD_NFC_HAL_SUFFIX)
+else
+    HAL_SUFFIX := pn54x.default
+endif
+
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_MODULE := nfc_nci.$(TARGET_DEVICE)
+LOCAL_MODULE := nfc_nci.$(HAL_SUFFIX)
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SRC_FILES := $(call all-subdir-c-files)  $(call all-subdir-cpp-files)
 LOCAL_SHARED_LIBRARIES := liblog libcutils libhardware_legacy libdl libhardware
