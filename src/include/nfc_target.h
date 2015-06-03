85998fdbee9f8a28183de292a8664021c823999e
Jizhou Liao <Jizhou.Liao@nxp.com>
2015-06-03 12:44:58 -0700

    Increase maximum AID routing buffer length and entry size.
    Support up to 50 AID routing entries or up to
    580 bytes in AID routing table
    
    Change-Id: I3582d0e7652e7be8e5b8d045d39b9a6dcf7b6888

--- src/include/nfc_target.h
+++ src/include/nfc_target.h
@@ -665,7 +665,7 @@
 
 /* Maximum number of AID entries per target_handle  */
 #ifndef NFA_EE_MAX_AID_ENTRIES
-#define NFA_EE_MAX_AID_ENTRIES      (32)
+#define NFA_EE_MAX_AID_ENTRIES      (50)
 #endif
 
 /* Maximum number of callback functions can be registered through NFA_EeRegister() */
