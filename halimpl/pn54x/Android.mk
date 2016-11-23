3cf6ea365993db8abf479b46163421aa78238050
Hendrik Hagendorn <git@finnq.de>
2016-11-23 18:50:54 +0100

    libnfc-nci: set NXP_CHIP_TYPE based on BOARD_NFC_CHIPSET
    * e.g. this will fix the firmware path for PN547C2 and PN548C2
    
    Change-Id: Idad4c3b31dfc9095b70920bec9e4d3828398c39f

--- halimpl/pn54x/Android.mk
+++ halimpl/pn54x/Android.mk
@@ -32,6 +32,14 @@ D_CFLAGS += -DPN551=3
 endif
 
 #### Select the CHIP ####
+ifeq ($(BOARD_NFC_CHIPSET),pn547)
+NXP_CHIP_TYPE := $(PN547C2)
+else ifeq ($(BOARD_NFC_CHIPSET),pn548)
+NXP_CHIP_TYPE := $(PN548C2)
+else
+NXP_CHIP_TYPE := $(PN551)
+endif
+
 ifeq ($(NXP_CHIP_TYPE),$(PN547C2))
 D_CFLAGS += -DNFC_NXP_CHIP_TYPE=PN547C2
 else ifeq ($(NXP_CHIP_TYPE),$(PN548C2))
