AOSPA/android_packages_modules_Nfc

Prevent integer overflow in NDEF_MsgValidate
Bug: 126200054
Test: Read a Ndef Tag
Change-Id: I156047fa8b6219a4d4d269f7ca720f9a0ee55e17

libnfc-nci/src/nfc/ndef/ndef_utils.c
@@ -23,6 +23,7 @@
  *
  ******************************************************************************/
 #include "ndef_utils.h"
+#include <log/log.h>
 #include <string.h>
 
 /*******************************************************************************
@@ -74,6 +75,7 @@ tNDEF_STATUS NDEF_MsgValidate(uint8_t* p_msg, uint32_t msg_len,
                               bool b_allow_chunks) {
   uint8_t* p_rec = p_msg;
   uint8_t* p_end = p_msg + msg_len;
+  uint8_t* p_new;
   uint8_t rec_hdr = 0, type_len, id_len;
   int count;
   uint32_t payload_len;
@@ -193,6 +195,13 @@ tNDEF_STATUS NDEF_MsgValidate(uint8_t* p_msg, uint32_t msg_len,
             p_rec_type[type_index] > NDEF_RTD_VALID_END)
           return (NDEF_MSG_INVALID_TYPE);
       }
+    }
+
+    /* Check for OOB */
+    p_new = p_rec + (payload_len + type_len + id_len);
+    if (p_rec > p_new || p_end < p_new) {
+        android_errorWriteLog(0x534e4554, "126200054");
+        return (NDEF_MSG_LENGTH_MISMATCH);
     }
 
     /* Point to next record */

LineageOS/android_external_libnfc-nci

src/nfc/ndef/ndef_utils.c
@@ -24,6 +24,7 @@
  *
  ******************************************************************************/
 #include <string.h>
+#include <log/log.h>
 #include "ndef_utils.h"
 
 /*******************************************************************************
@@ -80,6 +81,7 @@ tNDEF_STATUS NDEF_MsgValidate (UINT8 *p_msg, UINT32 msg_len, BOOLEAN b_allow_chu
 {
     UINT8   *p_rec = p_msg;
     UINT8   *p_end = p_msg + msg_len;
+    UINT8   *p_new;
     UINT8   rec_hdr=0, type_len, id_len;
     int     count;
     UINT32  payload_len;
@@ -185,6 +187,14 @@ tNDEF_STATUS NDEF_MsgValidate (UINT8 *p_msg, UINT32 msg_len, BOOLEAN b_allow_chu
         {
             if (type_len != 0)
                 return (NDEF_MSG_LENGTH_MISMATCH);
+        }
+
+        /* Check for OOB */
+        p_new = p_rec + (payload_len + type_len + id_len);
+        if (p_rec > p_new || p_end < p_new)
+        {
+            android_errorWriteLog(0x534e4554, "126200054");
+            return (NDEF_MSG_LENGTH_MISMATCH);
         }
 
         /* Point to next record */